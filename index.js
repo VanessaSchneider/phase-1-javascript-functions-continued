d861cb5524d95f14c5ad85667793f905b9b289f9
Vanessa Schneider <vanessajwade@gmail.com>
2022-01-11 18:27:35 -0500

    commit tree b3918182665fc98b0aa9105111f6c0cdae7dff91

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+
+    return `This Saturday, I want to ${activity}!`;
+
+}
+
+saturdayFun("bathe my dog");
+
+function mondayWork(activity = "go to the office") {return `This Monday, I will ${activity}.`}
+
+
+
+
+function wrapAdjective(style = "*") {
+    const innerFunction = function (adj = "special") {
+      return `You are ${style}${adj}${style}!`
+}
+
+return innerFunction;}
+
+undefined
+wrapAdjective ("%")("a dedicated programmer");
